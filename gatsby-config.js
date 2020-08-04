c149a9254646dea54d5b8face3bf44e92c3cdfe9
Michael Manhard <mmanhard93@gmail.com>
2020-08-04 13:45:16 -0500

    -Set up AWS S3 plugin and configure S3 bucket to deploy to.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Michael Manhard's Portfolio`,
+    description: `I'm Michael Manhard, a software engineer from Chicago. You can learn more about me on this site.`,
+    author: `Michael Manhard`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,11 +24,15 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/gatsby-icon.png`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: "mmanhard.com",
+        acl: null,
+      },
+    },
   ],
 }
