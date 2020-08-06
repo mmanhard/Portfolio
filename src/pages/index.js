8b4ef4ccc56fd0fd05fc114eaa55dde8e8d16f81
Michael Manhard <mmanhard93@gmail.com>
2020-08-06 15:05:16 -0500

    -Add Font Awesome.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin, faGithub } from "@fortawesome/free-brands-svg-icons"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
@@ -13,8 +15,19 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/projects/">Projects</Link>
-    <Link to="/contact/">Contact Me</Link>
+    <Link style={{margin: 10}} to="/projects/">
+      Projects
+    </Link>
+    <Link style={{margin: 10}} to="/contact/">
+      Contacts
+    </Link>
+
+    <a style={{margin: 10}} href="https://www.linkedin.com/in/michael-m-manhard/">
+      <FontAwesomeIcon style={{width: 30, height: 30}} icon={faLinkedin} />
+    </a>
+    <a style={{margin: 10}} href="https://github.com/mmanhard/">
+      <FontAwesomeIcon style={{width: 30, height: 30}} icon={faGithub} />
+    </a>
   </Layout>
 )
 
