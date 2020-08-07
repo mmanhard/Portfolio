5c7bb5659315c0f871fdcf04c8ca129936c71467
Michael Manhard <mmanhard93@gmail.com>
2020-08-06 20:37:12 -0500

    -Add 'now' and 'resume' page. Get rid of 'contact' page.

---
+++ src/pages/now.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+const Now = () => (
+  <Layout>
+    <h1>This is the now page</h1>
+  </Layout>
+)
+
+export default Now;
---
+++ src/pages/resume.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+const Resume = () => (
+  <Layout>
+    <h1>This is the resume page</h1>
+  </Layout>
+)
+
+export default Resume;
