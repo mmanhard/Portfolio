1bebe7f2f8c323932a2c293befa8deee339b0bb8
Michael Manhard <mmanhard93@gmail.com>
2020-08-11 15:30:01 -0500

    -Add some styling to projects page.

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -11,7 +11,7 @@ const Projects = () => {
     query {
       pain_control_img: file(relativePath: { eq: "Pain-Control-1.png" }) {
         childImageSharp {
-          fluid(maxWidth: 800) {
+          fluid(maxWidth: 600) {
             ...GatsbyImageSharpFluid
           }
         }
@@ -49,53 +49,160 @@ const Projects = () => {
 
   return (
     <Layout>
-      <h1>Projects</h1>
 
-      <>
-        <h2>Pain Control</h2>
-        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-          <Img fluid={data.pain_control_img.childImageSharp.fluid} />
-        </div>
-        <VideoLayout projectTitle={'painControl'} />
-        <a style={{margin: 10}} href="http://mypaincontroller.com/">View Live</a>
-        <a style={{margin: 10}} href="https://github.com/mmanhard/pain_control_app">View Source (FE)</a>
-        <a style={{margin: 10}} href="https://github.com/mmanhard/pain_control_backend">View Source (BE)</a>
-      </>
+      <div id={"projects_container"}>
 
-      <>
-        <h2>Touche</h2>
-        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-          <Img fluid={data.touche_img_1.childImageSharp.fluid} />
-        </div>
-        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-          <Img fluid={data.touche_img_2.childImageSharp.fluid} />
-        </div>
-        <a style={{margin: 10}} href="https://github.com/mmanhard/Touche">View Source</a>
-      </>
+        <LandscapeProjectItem
+          title={"Pain Control"}
+          description={"Short description of what the app does goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas libero neque, lobortis eget interdum non, laoreet id nisi."}
+          techStack={[
+              { section: "Front End",
+                tech: "Javascript + React + Express",
+              },
+              { section: "Back End",
+                tech: "Python + Flask + MongoDB",
+              },
+              {
+                section: "Tooling",
+                tech: "Heroku + Webpack",
+              }]}
+          image={data.pain_control_img}
+          links={[
+              {
+                href: "http://mypaincontroller.com/",
+                text: "LIVE",
+              },
+              {
+                href: "https://github.com/mmanhard/pain_control_app",
+                text: "SOURCE (FE)",
+              },
+              {
+                href: "https://github.com/mmanhard/pain_control_backend",
+                text: "SOURCE (BE)",
+              },
+              {
+                href: "https://github.com/mmanhard/pain_control_backend",
+                text: "SOURCE (BE)",
+              }]} />
 
-      <>
-        <h2>Fresh Focus</h2>
-        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-          <Img fluid={data.fresh_focus_img_1.childImageSharp.fluid} />
-        </div>
-        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-          <Img fluid={data.fresh_focus_img_2.childImageSharp.fluid} />
-        </div>
-        <VideoLayout projectTitle={'freshFocus'} />
-        <a style={{margin: 10}} href="https://github.com/mmanhard/FreshFocus">View Source</a>
-      </>
+        <hr />
 
-      <>
-        <h2>This Portfolio</h2>
-        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-          <Img fluid={data.pain_control_img.childImageSharp.fluid} />
-        </div>
-        <a style={{margin: 10}} href="http://mmanhard.com/">View Live</a>
-        <a style={{margin: 10}} href="https://github.com/mmanhard/Portfolio">View Source</a>
-      </>
+        <PortraitProjectItem
+          title={"Touche"}
+          description={"Short description of what the app does goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas libero neque, lobortis eget interdum non, laoreet id nisi."}
+          techStack={[
+              { section: "Front End",
+                tech: "React.JS",
+              },
+              { section: "Front End",
+                tech: "React.JS",
+              }]}
+          images={[data.touche_img_1, data.touche_img_2]}
+          links={[
+              {
+                href: "https://github.com/mmanhard/Touche",
+                text: "SOURCE",
+              }]} />
+
+        <hr />
+
+        <PortraitProjectItem
+          title={"Fresh Focus"}
+          description={"Short description of what the app does goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas libero neque, lobortis eget interdum non, laoreet id nisi."}
+          techStack={[
+              { section: "Front End",
+                tech: "React.JS",
+              },
+              { section: "Front End",
+                tech: "React.JS",
+              }]}
+          images={[data.fresh_focus_img_1, data.fresh_focus_img_2]}
+          links={[
+              {
+                href: "https://github.com/mmanhard/FreshFocus",
+                text: "SOURCE",
+              }]} />
+
+        <hr />
+
+        <PortraitProjectItem
+          title={"This Portfolio"}
+          description={"Short description of what the app does goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas libero neque, lobortis eget interdum non, laoreet id nisi."}
+          techStack={[
+              { section: "Front End",
+                tech: "React.JS",
+              },
+              { section: "Front End",
+                tech: "React.JS",
+              }]}
+          images={[data.pain_control_img]}
+          links={[
+              { href: "http://mmanhard.com/",
+                text: "LIVE",
+              },
+              {
+                href: "https://github.com/mmanhard/Portfolio",
+                text: "SOURCE",
+              }]} />
+
+      </div>
+
+      <div className={"column_ctr title_container"}>
+        <h1>PROJECTS.</h1>
+      </div>
 
     </Layout>
   );
 }
+
+const LandscapeProjectItem = ({title, description, techStack, image, links}) => (
+  <div className={"column_ctr landscape_container"}>
+    <div className={"landscape_content"}>
+      <div className={"landscape_description"}>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        {techStack.map(({ section, tech}) => (
+          <p style={{marginBottom: 0}}>{`${section}:\t${tech}`}</p>
+        ))}
+      </div>
+
+      <div className={"landscape_img"}>
+        <Img className={"project_img"} fluid={image.childImageSharp.fluid} />
+      </div>
+    </div>
+
+    <div>
+      {links.map(({href, text}) => (
+        <a className={"project_btn"} href={href}>
+          <div className={"column_ctr"} style={{height: 16}}>VIEW</div>
+          <div className={"column_ctr"} style={{height: 16}}>{text}</div>
+        </a>
+      ))}
+    </div>
+  </div>
+)
+
+const PortraitProjectItem = ({title, description, techStack, images, links}) => (
+  <div className={"portrait_container"}>
+    <div style={{flex: 1}}>
+      <h2>{title}</h2>
+      <p>{description}</p>
+      {techStack.map(({ section, tech}) => (
+        <p>{`${section}:\t${tech}`}</p>
+      ))}
+      {links.map(({href, text}) => (
+        <a className={"project_btn"} href={href}>
+          <div className={"column_ctr"} style={{height: 16}}>VIEW</div>
+          <div className={"column_ctr"} style={{height: 16}}>{text}</div>
+        </a>
+      ))}
+    </div>
+    {images.map((image, index) => (
+      <div className={index > 0 ? "second_img" : ""} style={{ width: `165px`, marginLeft: 20, marginBottom: `1.45rem` }}>
+        <Img className={"project_img"} fluid={image.childImageSharp.fluid} />
+      </div>
+    ))}
+  </div>
+)
 
 export default Projects;
