088b5a578e48650f27e9bfc40022e98bc2c372e3
Michael Manhard <mmanhard93@gmail.com>
2020-08-13 14:28:09 -0500

    -Implement layout component for videos.

--- src/components/video_layout.js
+++ src/components/video_layout.js
@@ -1,17 +1,30 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faTimesCircle } from "@fortawesome/free-solid-svg-icons"
+
 import "./layout.css"
 
-const VideoLayout = ({ projectTitle }) => {
+const VideoLayout = ({ projectTitle, closeVideo }) => {
+  const { width, height } = projectDimRatios[projectTitle];
 
   return (
-    <>
-      <iframe width="560" height="315"
-        src={projectToURL[projectTitle]}
-        frameborder="0"
-        allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen={true} />
-    </>
+    <div className={'video_overlay column_ctr'}>
+
+      <div className={'column_space video_content'}>
+        <div id={"video_container"} style={{paddingTop: height, width}}>
+          <iframe
+            src={projectToURL[projectTitle]}
+            frameBorder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen={true} />
+        </div>
+        <h1 style={{marginBottom: 20}}>{projectTitle}</h1>
+        <button onClick={closeVideo} style={{marginBottom: 20}}>
+          <FontAwesomeIcon style={{width: 30, height: 30, color: '#fff'}} icon={faTimesCircle} />
+        </button>
+      </div>
+    </div>
   )
 }
 
@@ -20,8 +33,24 @@ VideoLayout.propTypes = {
 }
 
 const projectToURL = {
-  painControl: "https://www.youtube.com/embed/md52E5fJaUE",
-  freshFocus: "https://www.youtube.com/embed/yAW-TZSDn6A"
+  'Pain Control': "https://www.youtube.com/embed/md52E5fJaUE?autoplay=1",
+  'Touche': "https://www.youtube.com/embed/md52E5fJaUE?autoplay=1",
+  'Fresh Focus': "https://www.youtube.com/embed/yAW-TZSDn6A?autoplay=1"
+}
+
+const projectDimRatios = {
+  'Pain Control': {
+    height: '60%',
+    width: '90%',
+  },
+  'Touche': {
+    height: '60%',
+    width: '90%',
+  },
+  'Fresh Focus': {
+    height: '60%',
+    width: '50%',
+  },
 }
 
 export default VideoLayout;
