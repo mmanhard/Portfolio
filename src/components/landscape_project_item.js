ac2fd6e4a3228b314ca3c7499ef9b7eec8a7b88f
Michael Manhard <mmanhard93@gmail.com>
2020-09-02 16:53:16 -0500

    -Fix project layout error on Safari.

--- src/components/landscape_project_item.js
+++ src/components/landscape_project_item.js
@@ -2,46 +2,48 @@ import React from "react"
 import Img from "gatsby-image"
 
 const LandscapeProjectItem = ({title, platform, type, description, techStack, image, links, displayVideo}) => (
-  <div className={"column_ctr landscape_container"}>
-    <div className={"landscape_content"}>
-      <div className={"landscape_description"}>
+  <div>
+    <div className={"column_ctr landscape_container"}>
+      <div className={"landscape_content"}>
+        <div className={"landscape_description"}>
 
-        <h2 style={{marginBottom: 4}}>{title}<span>{platform}</span></h2>
-        <h5 style={{marginBottom: 4}}>{type}</h5>
+          <h2 style={{marginBottom: 4}}>{title}<span>{platform}</span></h2>
+          <h5 style={{marginBottom: 4}}>{type}</h5>
 
-        <p className={"project_description"}>{description}</p>
+          <p className={"project_description"}>{description}</p>
 
-        {techStack.map(({ section, tech}) => (
-          <p key={title + section}
-            className={"project_description"}
-            style={{marginBottom: 0}}>
-            <span>{`${section}:    `}</span>
-            <strong>{tech}</strong>
-          </p>
-        ))}
+          {techStack.map(({ section, tech}) => (
+            <p key={title + section}
+              className={"project_description"}
+              style={{marginBottom: 0}}>
+              <span>{`${section}:    `}</span>
+              <strong>{tech}</strong>
+            </p>
+          ))}
 
+        </div>
+
+        {image && <div className={"landscape_img"}>
+          <Img className={"project_img"} fluid={image.childImageSharp.fluid} />
+        </div>}
       </div>
 
-      {image && <div className={"landscape_img"}>
-        <Img className={"project_img"} fluid={image.childImageSharp.fluid} />
-      </div>}
-    </div>
-
-    <div className={"row_space"} style={{flexWrap: 'wrap', marginBottom: 0}}>
-      {links.map(({href, text}) => (
-        <a key={href}
-          className={"project_btn"}
-          href={href}
-          target="_blank"
-          rel="noopener noreferrer">
+      <div className={"row_space"} style={{flexWrap: 'wrap', marginBottom: 0}}>
+        {links.map(({href, text}) => (
+          <a key={href}
+            className={"project_btn"}
+            href={href}
+            target="_blank"
+            rel="noopener noreferrer">
+            <div className={"column_ctr"} style={{height: 16}}>VIEW</div>
+            <div className={"column_ctr"} style={{height: 16}}>{text}</div>
+          </a>
+        ))}
+        {displayVideo && <button onClick={() => { displayVideo(title)}} className={"project_btn"}>
           <div className={"column_ctr"} style={{height: 16}}>VIEW</div>
-          <div className={"column_ctr"} style={{height: 16}}>{text}</div>
-        </a>
-      ))}
-      {displayVideo && <button onClick={() => { displayVideo(title)}} className={"project_btn"}>
-        <div className={"column_ctr"} style={{height: 16}}>VIEW</div>
-        <div className={"column_ctr"} style={{height: 16}}>DEMO</div>
-      </button>}
+          <div className={"column_ctr"} style={{height: 16}}>DEMO</div>
+        </button>}
+      </div>
     </div>
   </div>
 )
--- src/components/portrait_project_item.js
+++ src/components/portrait_project_item.js
@@ -2,48 +2,50 @@ import React from "react"
 import Img from "gatsby-image"
 
 const PortraitProjectItem = ({title, platform, type, description, techStack, images, links, displayVideo, isMobile}) => (
-  <div className={"portrait_container"}>
-    <div className={"portait_content"}>
+  <div>
+    <div className={"portrait_container"}>
+      <div className={"portait_content"}>
 
-      <h2 style={{marginBottom: 4}}>{title}<span>{platform}</span></h2>
-      <h5 style={{marginBottom: 4}}>{type}</h5>
+        <h2 style={{marginBottom: 4}}>{title}<span>{platform}</span></h2>
+        <h5 style={{marginBottom: 4}}>{type}</h5>
 
-      <p className={"project_description"}>{description}</p>
+        <p className={"project_description"}>{description}</p>
 
-      {techStack.map(({ section, tech}) => (
-        <p key={title + section}
-          className={"project_description"}
-          style={{marginBottom: 0}}>
-          <span>{`${section}:    `}</span>
-          <strong>{tech}</strong>
-        </p>
-      ))}
+        {techStack.map(({ section, tech}) => (
+          <p key={title + section}
+            className={"project_description"}
+            style={{marginBottom: 0}}>
+            <span>{`${section}:    `}</span>
+            <strong>{tech}</strong>
+          </p>
+        ))}
 
-      <div className={'visible_small'}>
+        <div className={'visible_small'}>
+          <PortraitImages title={title} images={images} isMobile={isMobile} />
+        </div>
+
+        <div className={"row_space portrait_btns"}>
+          {links.map(({href, text}) => (
+            <a key={href}
+              className={"project_btn"}
+              href={href}
+              target="_blank"
+              rel="noopener noreferrer">
+              <div className={"column_ctr"} style={{height: 16}}>VIEW</div>
+              <div className={"column_ctr"} style={{height: 16}}>{text}</div>
+            </a>
+          ))}
+          {displayVideo && <button onClick={() => { displayVideo(title)}} className={"project_btn"}>
+            <div className={"column_ctr"} style={{height: 16}}>VIEW</div>
+            <div className={"column_ctr"} style={{height: 16}}>DEMO</div>
+          </button>}
+        </div>
+
+      </div>
+
+      <div className={'not_visible_small'}>
         <PortraitImages title={title} images={images} isMobile={isMobile} />
       </div>
-
-      <div className={"row_space portrait_btns"}>
-        {links.map(({href, text}) => (
-          <a key={href}
-            className={"project_btn"}
-            href={href}
-            target="_blank"
-            rel="noopener noreferrer">
-            <div className={"column_ctr"} style={{height: 16}}>VIEW</div>
-            <div className={"column_ctr"} style={{height: 16}}>{text}</div>
-          </a>
-        ))}
-        {displayVideo && <button onClick={() => { displayVideo(title)}} className={"project_btn"}>
-          <div className={"column_ctr"} style={{height: 16}}>VIEW</div>
-          <div className={"column_ctr"} style={{height: 16}}>DEMO</div>
-        </button>}
-      </div>
-
-    </div>
-
-    <div className={'not_visible_small'}>
-      <PortraitImages title={title} images={images} isMobile={isMobile} />
     </div>
   </div>
 )
